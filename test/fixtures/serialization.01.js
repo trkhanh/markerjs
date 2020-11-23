fea87fff22013880b2eb932f48fb8ddbabac48f4
Khanh <tran_khanh@outlook.com>
2020-11-23 14:31:49 +0700

    update test case for removeing

---
+++ test/fixtures/serialization.01.js
@@ -0,0 +1,13 @@
+fixtures.register('01.s11n.base', function () {
+    SPAN(
+        'AAA',
+        SPAN(
+            'CCC',
+            SPAN('Lorem ipsum dolor sit amet'),
+            SPAN('consectetur adipiscit'),
+            SPAN('elit.')
+        ),
+        SPAN('DDD'),
+        'BBB'
+    )
+});
--- test/specs/removing.spec.js
+++ test/specs/removing.spec.js
@@ -5,7 +5,15 @@ describe("Removing highlights", () => {
 
   var marker;
 
-  fixtures.loadFiles(["removing.01"]);
+  fixtures.loadFiles([
+    "removing.01",
+    "removing.02",
+    "removing.03",
+    "removing.04",
+    "removing.05",
+    "removing.06",
+    "removing.07",
+  ]);
 
   beforeEach(function () {
     marker = sandbox.init();
@@ -49,10 +57,117 @@ describe("Removing highlights", () => {
       );
     });
   }
+
   testRemoving({
     title: "use case #01",
     fixture: "01.preremove",
     expectedTextNodesCount: 1,
     expectedHTMLFixture: "01.postremove.all",
+  });
+
+  testRemoving({
+    title: "use case #02",
+    fixture: "02.preremove",
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "02.postremove.all",
+  });
+  testRemoving({
+    title: "use case #03",
+    fixture: "02.preremove",
+    which: ["h1"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "02.postremove.all",
+  });
+
+  testRemoving({
+    title: "use case #04",
+    fixture: "02.preremove",
+    which: ["h2"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "02.postremove.h2",
+  });
+
+  testRemoving({
+    title: "use case #05",
+    fixture: "03.preremove",
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "03.postremove.all",
+  });
+  testRemoving({
+    title: "use case #06",
+    fixture: "03.preremove",
+    which: ["h1"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "03.postremove.all",
+  });
+  testRemoving({
+    title: "use case #07",
+    fixture: "03.preremove",
+    which: ["h2"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "03.postremove.h2",
+  });
+
+  testRemoving({
+    title: "use case #08",
+    fixture: "04.preremove",
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "04.postremove.all",
+  });
+  testRemoving({
+    title: "use case #09",
+    fixture: "04.preremove",
+    which: ["h1"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "04.postremove.all",
+  });
+  testRemoving({
+    title: "use case #10",
+    fixture: "04.preremove",
+    which: ["h2"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "04.postremove.h2",
+  });
+
+  testRemoving({
+    title: "use case #11",
+    fixture: "05.preremove",
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "05.postremove.all",
+  });
+  testRemoving({
+    title: "use case #12",
+    fixture: "05.preremove",
+    which: ["h1"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "05.postremove.h1",
+  });
+  testRemoving({
+    title: "use case #13",
+    fixture: "05.preremove",
+    which: ["h2"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "05.postremove.h2",
+  });
+  testRemoving({
+    title: "use case #14",
+    fixture: "05.preremove",
+    which: ["h3"],
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "05.postremove.h3",
+  });
+
+  testRemoving({
+    title: "use case #15",
+    fixture: "06.preremove",
+    expectedTextNodesCount: 1,
+    expectedHTMLFixture: "06.postremove.all",
+  });
+
+  testRemoving({
+    title: "use case #17",
+    fixture: "07.preremove",
+    expectedTextNodesCount: 7,
+    expectedHTMLFixture: "07.postremove.all",
   });
 });
